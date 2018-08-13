f55f34dca18bb5cdc5a7fdd79c9db7bbac404fbf
viestur8 <twotigers@vivaldi.net>
2018-08-13 17:31:03 -0400

    Created cookies and cakes

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
-require 'sinatra'
-require './staff.rb'
+  require 'sinatra'
+  require 'swapir'
+  require './products.rb'
+
+  macaroon = QuickDeath.new('macaroon', '/cookies/3macaroons.jpeg', '$9.99', "Our famous Three Flavor Macaroons - Choice of Cyanide, Arsenic or Both")
+
+  dare = QuickDeath.new('Truth or dare', '/cookies/truth_or_dare_cookies.jpeg', '$19.99', "For a more Exciting and Lethal Game night")
+  bery = QuickDeath.new('Happy Berry', '/cookies/raspbery_on_top.jpeg', '$19.99', "A Classy night out..")
+
+
+my_cookies = {
+  macaroon: macaroon,
+  dare: dare,
+  bery: bery
+  }
+ happy = SlowDeath.new('Happy Birthday, ...', '/cakes/birthday_cake.jpg', '$199.99', "For a more Exciting Day in the Office")
+
+ chair = SlowDeath.new('LZ Boy', '/cakes/chair.jpg', '$199.99', "After a long day at the Office")
+
+ wedding1 = SlowDeath.new('True Love', '/cakes/wedding.jpg', '$199.99', "True Love Knows No Bounds")
+
+ my_cakes = {
+   happy: happy,
+   chair: chair,
+   wedding1: wedding1
+ }
+
 
 get '/' do
-    'Jonestown bakery! where you can have various different suicideable cookies and drinks!'
+    erb :home
+end
+
+get '/cookies' do
+      @cookies = my_cookies
+      puts "HELLO WORLD"
+      puts @cookies
+      puts "HELLO WORLD"
+    erb :cookies
+end
+
+get '/cakes' do
+      @cakes = my_cakes
+      puts "HELLO WORLD"
+      puts @cakes
+      puts "HELLO WORLD"
+    erb :cakes
 end
---
+++ products.rb
@@ -0,0 +1,22 @@
+class QuickDeath
+    attr_accessor :name,:photo,:price,:description
+
+    def initialize(name, photo, price, description)
+        @name = name
+        @photo = photo
+        @position = price
+        @description = description
+    end
+
+end
+class SlowDeath
+    attr_accessor :name,:photo,:price,:description
+
+    def initialize(name, photo, price, description)
+        @name = name
+        @photo = photo
+        @position = price
+        @description = description
+    end
+
+end
